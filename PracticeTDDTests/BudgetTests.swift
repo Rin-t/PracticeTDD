cade7f9c76aa840accc6b0227cd638e85d1bf704
Rin-t <rin.t.4921@gmail.com>
2023-06-05 21:28:49 +0900

    add: 予算管理のテスト雛形を追加

---
+++ PracticeTDDTests/BudgetTests.swift
@@ -0,0 +1,28 @@
+//
+//  BudgetTests.swift
+//  PracticeTDDTests
+//
+//  Created by Rin on 2023/06/05.
+//
+
+import XCTest
+
+final class BudgetTests: XCTestCase {
+
+    func test_週の残りの予算を含んでいること() {
+
+    }
+
+    func test_日の残りの予算を含んでいること() {
+
+    }
+
+    func test_週の残り予算から使用した金額が引かれていること() {
+
+    }
+
+    func test_日の残り予算から使用した金額が引かれていること() {
+
+    }
+
+}
