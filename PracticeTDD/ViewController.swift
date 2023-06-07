aea80f4d055e18cd9e08f853577695a1c34f1e50
Rin-t <rin.t.4921@gmail.com>
2023-06-07 21:27:16 +0900

    add: ViewControllerの処理を追加

--- PracticeTDD/ViewController.swift
+++ PracticeTDD/ViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var totalBudgetTextField: UITextField!
+    @IBOutlet weak var transactionTextField: UITextField!
+    @IBOutlet weak var weeklyRemainingLabel: UILabel!
+    @IBOutlet weak var todayRemainingLabel: UILabel!
+
+    var budget: Budget?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        transactionTextField.delegate = self
+        totalBudgetTextField.delegate = self
     }
 
+    private func updateBudgetLabels() {
+        weeklyRemainingLabel.text = budget?.weeklyRemaining.moneyString
+        todayRemainingLabel.text = budget?.dailyRemaining.moneyString
+    }
 
+}
+
+extension ViewController: UITextFieldDelegate {
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        guard let total = Decimal(string: textField.text!) else {
+            return true
+        }
+
+        if textField == totalBudgetTextField {
+            budget = Budget(total: total)
+        }
+
+        if textField == transactionTextField {
+            budget?.addTransaction(amount: total, timestamp: Date())
+        }
+
+        updateBudgetLabels()
+        return true
+    }
 }
 
